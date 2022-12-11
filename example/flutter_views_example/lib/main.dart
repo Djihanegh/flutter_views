4279ae9df629df74b33a8e3d9e87849a0989a654
djihane Ghilani <djihaneghilani@gmail.com>
2022-12-11 17:34:49 +0100

    NEW: adding the example app to test the package

---
+++ example/flutter_views_example/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_views/flutter_views.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+            color: Colors.transparent,
+            child: FlutterViews(
+              spacing: 2,
+              shape: BoxShape.circle,
+              border: BorderRadius.circular(20),
+              views: [],
+            )),
+      ),
+    );
+  }
+}
